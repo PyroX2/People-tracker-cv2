180b801a777e9b457ff82ad1b3260d39c4767b0b
Jakub Wilk <kubawilk63@gmail.com>
2026-01-12 15:25:20 +0100

    Use new procedure for bbox matching between detections and tracks

--- kcf_yolo.py
+++ kcf_yolo.py
@@ -5,19 +5,21 @@ from time import perf_counter
 from kcf_tracker import TrackerKCF_create, TrackerReliabilityKCF_create
 
 # --- CONFIGURATION ---
-SCALE_FACTOR = 0.5
-DETECTION_INTERVAL = 2
-CONFIDENCE_THRESHOLD = 0.4
-DISTANCE_THRESHOLD = 30  # Pixels. If center distance > this, it's a new object.
+SCALE_FACTOR = 1
+DETECTION_INTERVAL = 1
+CONFIDENCE_THRESHOLD = 0.35
+MISSES_ALLOWED = 5
+IOU_THRESHOLD = 0.3
+DISTANCE_THRESHOLD = 50  # Pixels. If center distance > this, it's a new object.
 # TRACKER_CLASS = cv2.TrackerCSRT_create
 # TRACKER_CLASS = cv2.TrackerKCF_create
 TRACKER_CLASS = TrackerReliabilityKCF_create
 
 # Create yolo model for detection people
-model = YOLO("models/yolov8x.pt") 
+model = YOLO("models/yolov8l.pt") 
 
 # Create video reader
-video = cv2.VideoCapture("data/video_4.mp4")
+video = cv2.VideoCapture("data/aerial-view-of-crowds-2.mp4")
 
 trackers = []   # List for storing all trackers
 frame_count = 0 # Number of frames processed
@@ -27,9 +29,9 @@ ids_in_roi = []  # Ids of objects that entered ROI
 # Function that returns center point of bounding box
 def get_center(bbox):
     """Calculates the center point (x, y) of a bounding box."""
-    x, y, w, h = bbox
-    cx = x + w / 2
-    cy = y + h / 2
+    x1, y1, x2, y2 = bbox
+    cx = int((x2 + x1) / 2)
+    cy = int((y2 + y1) / 2)
     return cx, cy
 
 # Function that checks if point is inside the ROI
@@ -40,6 +42,32 @@ def point_in_roi(roi, point):
         return True
     else:
         return False
+    
+
+# Function that returns IoU of two bounding boxes in format [x1, y1, x2, y2]
+def get_iou(bbox1, bbox2):
+    x_left = max(bbox1[0], bbox2[0])
+    y_top = max(bbox1[1], bbox2[1])
+    x_right = min(bbox1[2], bbox2[2])
+    y_bottom = min(bbox1[3], bbox2[3])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0  # No overlap
+
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    bbox1_area = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+    bbox2_area = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
+
+    union_area = bbox1_area + bbox2_area - intersection_area
+
+    return intersection_area / union_area if union_area > 0 else 0.0
+
+def xyxy2xywh(bbox):
+    return [bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3]-bbox[1]]
+
+def xywh2xyxy(bbox):
+    return [bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3]]
 
 
 ret, first_frame = video.read() # Read first frame
@@ -65,79 +93,128 @@ while True:
         
     if frame_count % DETECTION_INTERVAL == 0:
         # Detect people using YOLO
-        results = model(frame, conf=CONFIDENCE_THRESHOLD, verbose=False, classes=[0], imgsz=640)
+        results = model(frame, conf=CONFIDENCE_THRESHOLD, verbose=False, classes=[0], imgsz=640, iou=0.2)
         
         # Parse YOLO detections into a clean list of [x, y, w, h]
         detections = []
         for result in results:
-            for box in result.boxes:    # Get the left top corner of bbox, width and height 
+            for box in result.boxes:
                 x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
-                w = int(x2 - x1)
-                h = int(y2 - y1)
-                detections.append((int(x1), int(y1), w, h))
+                detections.append((int(x1), int(y1), int(x2), int(y2)))
 
-        # 2. MATCHING (Euclidean Distance)
+        """
+        BBOX cleaning procedure:
+            - check IoU between detection bounding box and existing track
+                - if it is greater than THRESHOLD it is the same object so the ID stays the same
+                    This is check for all trackers even those that were lost previously
+                - if not we check if distance between the center of detection bounding box and center of tracks that 
+                    were not updated is smaller than DISTANCE_THRESHOLD. This allows for restoration of lost tracks
+                - if bounding box was not matched using IoU or distance we assume that it is a new object and we should
+                    create a new tracker for it.
+        """
+        
+
         # Mark all current trackers as "not updated" for this frame
         for obj in trackers:
             obj['updated'] = False
 
+        not_matched_dets = []
+
+        # Match detection bboxes to existing tracking bboxes
         for det_box in detections:
-            det_center = get_center(det_box)
-            
             matched_obj = None
-            min_dist = float('inf')
+            best_iou = 0
 
             # Find the closest existing tracker
             for obj in trackers:
-                # If we don't have a last_bbox (newly added), skip or assume center
-                if 'last_bbox' not in obj: continue
-                
-                tr_center = get_center(obj['last_bbox'])
-                
-                # Calculate Euclidean Distance
-                dist = math.hypot(det_center[0] - tr_center[0], det_center[1] - tr_center[1])
-                
-                if dist < min_dist:
-                    min_dist = dist
+                if obj['updated']: continue # Prevent assigning multiple det bboxes to one track bbox
+                track_bbox = obj['last_bbox']
+                iou = get_iou(det_box, track_bbox)
+                if iou > best_iou:
                     matched_obj = obj
+                    best_iou = iou
 
-            # DECISION: Is it the same object?
-            if matched_obj and min_dist < DISTANCE_THRESHOLD:
+            if matched_obj and best_iou > IOU_THRESHOLD:
                 # YES -> Update existing tracker (Re-init to fix drift)
                 # We do NOT increment the ID, we keep matched_obj['id']
-                if not matched_obj['updated']: # Prevent multiple detections claiming one tracker
-                    matched_obj['tracker'] = TRACKER_CLASS()
-                    matched_obj['tracker'].init(frame, det_box)
-                    matched_obj['last_bbox'] = det_box
-                    matched_obj['updated'] = True
+                matched_obj['tracker'] = TRACKER_CLASS()
+                matched_obj['tracker'].init(frame, xyxy2xywh(det_box))
+                matched_obj['last_bbox'] = det_box
+                matched_obj['updated'] = True
             else:
-                # NO -> It's a new object
+                not_matched_dets.append(det_box)
+
+        not_matched_trackers = [obj for obj in trackers if not obj['updated']]
+
+        # Match using euclidian distance
+        for det_box in not_matched_dets:
+            det_center = get_center(det_box)
+
+            det_img = frame[det_box[1]:det_box[3], det_box[0]:det_box[2]]
+
+            matched_obj = None
+            min_dist = float('inf')
+
+            for obj in not_matched_trackers:
+                track_center = get_center(obj['last_bbox'])
+                dist = math.hypot(track_center[0]-det_center[0], track_center[1]-det_center[1])
+                
+                if dist < min_dist:
+                    matched_obj=obj
+                    min_dist = dist
+
+                tracker_img = frame[obj['last_bbox'][1]:obj['last_bbox'][3], obj['last_bbox'][0]:obj['last_bbox'][2]]
+            
+            if matched_obj and min_dist < DISTANCE_THRESHOLD:
+            
+                matched_obj['tracker'] = TRACKER_CLASS()
+                matched_obj['tracker'].init(frame, xyxy2xywh(det_box))
+                matched_obj['last_bbox'] = det_box
+                matched_obj['updated'] = True
+            else:
+                # Create new object
                 new_tracker = TRACKER_CLASS()
-                new_tracker.init(frame, det_box)
+                new_tracker.init(frame, xyxy2xywh(det_box))
                 trackers.append({
                     'tracker': new_tracker,
                     'id': global_id_counter,
                     'color': (0, 255, 0),
                     'last_bbox': det_box,
-                    'updated': True
+                    'updated': True,
                 })
                 global_id_counter += 1
-                
-        # Optional: Prune trackers that were NOT updated by YOLO?
-        # If you want to kill trackers that YOLO didn't see, uncomment below:
+
+        cleaned_trackers = []
+        for obj in trackers:
+            if obj['updated']:
+                obj['misses_counter'] = 0
+                cleaned_trackers.append(obj)
+            else:
+                obj['misses_counter'] += 1
+                if obj['misses_counter'] > MISSES_ALLOWED:
+                    continue
+                cleaned_trackers.append(obj)
+        trackers = cleaned_trackers
+
+
         # trackers = [obj for obj in trackers if obj['updated']]
+            
             
     # else:
     # TRACKING LOOP (Intermediate frames)
     for obj in trackers:
         success, bbox = obj['tracker'].update(frame)
+        # success = True
+        # bbox = obj['last_bbox']
+
+        bbox = xywh2xyxy(bbox)
         
         if success:
             # Store bbox for the next distance calculation
             obj['last_bbox'] = bbox
             
             p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+            p2 = (int(bbox[2]), int(bbox[3]))
             cv2.rectangle(frame, p1, p2, obj['color'], 2, 1)
             
             text = f"ID: {obj['id']}"
